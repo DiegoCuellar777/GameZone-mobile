b73a9d165d4821fb2921c3e13482ae51af337dae
FranciscoPedicino <franciscopedi91@gmail.com>
2023-06-13 17:24:42 -0300

    buscador

--- App.js
+++ App.js
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, StyleSheet, View, TextInput, TouchableOpacity, Alert, ScrollView, Image } from 'react-native';
-import { Tab, Text, TabView, Input, Icon } from '@rneui/themed';
-import logo from './assets/luis.png'
-import axios from 'axios';
-import api from './api'
-import Modal from 'react-native-modal';
-import GameList from './src/components/GameList';
-import inputs_filter_actions from './src/store/actions/inputs_filters'
-import {useSelector,useDispatch,} from 'react-redux'
-import { Provider } from 'react-redux';
-import store1 from './src/store/store'
-=======
 import React from 'react';
 import Navigation from './src/navigation/Navigation';
 import { NavigationContainer } from '@react-navigation/native';
->>>>>>> 5d65069151d30080e46bcbced12a133d30f7b7c8
 
 
-
-
-const { inputs_filter } = inputs_filter_actions
 
 export default function App() {
 
--- src/components/GamesCard.js
+++ src/components/GamesCard.js
@@ -5,56 +5,60 @@ import Favorite from './Favorite'
 
 
 export default function GamesCard(props) {
-  const {games} = props
+  const { games } = props
   const navigation = useNavigation()
 
   const gameDetails = () => {
-    navigation.navigate("Details", {title:games.title, photo:games.cover_photo, description:games.description, company:games?.company_id?.name})
+    navigation.navigate("Details", { title: games.title, photo: games.cover_photo, description: games.description, company: games?.company_id?.name })
   }
 
   return (
-    <TouchableWithoutFeedback onPress={gameDetails} style={styles.touchable}>
-      <View style={styles.card}>
-        <View style={styles.spacing}>
-          <View style={styles.bgStyles}>
-            <View style={{width:"65%", paddingLeft:20, justifyContent:"space-between"}}>
-              <View style={{width:"100%", flexDirection:'row', justifyContent:"space-between", alignItems:"center"}}>
-                <Text style={{color:"white", fontWeight:"bold", fontSize:18 }}>{games?.title}</Text>
-                <Favorite _id={games?._id}/>
+    <>
+     
+      <TouchableWithoutFeedback onPress={gameDetails} style={styles.touchable}>
+      
+        <View style={styles.card}>
+          <View style={styles.spacing}>
+            <View style={styles.bgStyles}>
+              <View style={{ width: "65%", paddingLeft: 20, justifyContent: "space-between" }}>
+                <View style={{ width: "100%", flexDirection: 'row', justifyContent: "space-between", alignItems: "center" }}>
+                  <Text style={{ color: "white", fontWeight: "bold", fontSize: 18 }}>{games?.title}</Text>
+                  <Favorite _id={games?._id} />
+                </View>
+                <Text style={{ color: "#c6cbcf", fontWeight: "bold", fontSize: 16 }}>{games?.company_id?.name?.toUpperCase()}</Text>
+                <View>
+                  <Text style={{ fontWeight: "bold", color: "grey" }}>Price</Text>
+                  <Text style={{ color: "white", fontWeight: "bold", fontSize: 25 }}>${games?.price}</Text>
+                </View>
               </View>
-              <Text style={{color:"#c6cbcf", fontWeight:"bold", fontSize:16}}>{games?.company_id?.name?.toUpperCase()}</Text>
-              <View>
-                <Text style={{fontWeight:"bold", color:"grey"}}>Price</Text>
-                <Text style={{color:"white", fontWeight:"bold", fontSize:25}}>${games?.price}</Text>
-              </View>
+              <Image
+                source={{ uri: `${games?.cover_photo}` }}
+                style={{ width: "35%", height: 200 }}>
+              </Image>
             </View>
-            <Image
-              source={{uri: `${games?.cover_photo}`}}
-              style={{width: "35%", height:200}}>
-            </Image>
           </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-  touchable:{
-    flex:1,
-    flexDirection:"row"
+  touchable: {
+    flex: 1,
+    flexDirection: "row"
   },
-  card:{
+  card: {
     width: "100%",
-  
+
   },
-  spacing:{
-    padding:10,
+  spacing: {
+    padding: 10,
   },
-  bgStyles:{
-    flex:1,
-    flexDirection:"row-reverse",
-    backgroundColor:"#2488ed30",
+  bgStyles: {
+    flex: 1,
+    flexDirection: "row-reverse",
+    backgroundColor: "#2488ed30",
     padding: 15
   }
 }) 
